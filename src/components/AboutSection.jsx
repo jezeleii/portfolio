1f22c2fd1e462a38ff9b67cb272e6fc23abfd867
jezeleii <jmanangan.2022@scis.smu.edu.sg>
2026-01-13 16:07:20 +0800

    update cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -25,7 +25,7 @@ export const AboutSection = () => {
                             Get in Touch
                         </a>
                         <a
-                            href="private/jezelei_manangan_cv.pdf"
+                            href="/jezelei_manangan_cv.pdf"
                             className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                             download
                         >
